8b46584ab5e99f6ee81108e139700c08ab1b106d
Renzo Schindeler <renzo@schindeler.nl>
2018-11-19 12:42:19 +0100

    Class met init functie

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace OptimaliserenPracticum
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			SimulatedAnnealing SA = new SimulatedAnnealing();
+			SA.Init();
 		}
 	}
+
+	class SimulatedAnnealing
+	{
+		// Initialize the program
+		public void Init()
+		{
+
+		}
+	}
+
+
 }
