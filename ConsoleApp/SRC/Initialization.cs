628908392b917194a016d7a740b3981321e0414c
Hidde Veer <hidde.veer@gmail.com>
2018-11-21 16:14:15 +0100

    Nieuwe push pas op!

---
+++ ConsoleApp/SRC/Initialization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Initialization
+    {
+        string localPath;
+        string distPath;
+        public Initialization()
+        {
+            localPath = Directory.GetCurrentDirectory();
+            distPath = "dist.txt";
+        }
+
+        public Tuple<int[,], int[,]> GetMatrix()
+        {
+            string[] input = File.ReadAllLines(localPath + distPath);
+            int inputLength = input.Length;
+            int start, end, dist, time;
+            int[,] distMatrix = new int[inputLength, inputLength];
+            int[,] timeMatrix = new int[inputLength, inputLength];
+            // The dist/time from each company to itself = 0;
+            for (int i = 0; i < inputLength; i++)
+            {
+                distMatrix[i, i] = 0;
+                timeMatrix[i, i] = 0;
+            }
+            // Read each line one by one and process it
+            for (int i = 0; i < inputLength; i++)
+            {
+                string[] line = input[i].Split(';');
+                start = int.Parse(line[0]);
+                end = int.Parse(line[1]);
+                dist = int.Parse(line[2]);
+                time = int.Parse(line[3]);
+                distMatrix[start, end] = dist;
+                timeMatrix[start, end] = time;
+            }
+            return new Tuple<int[,], int[,]>(distMatrix, timeMatrix);
+        }
+    }
+}
