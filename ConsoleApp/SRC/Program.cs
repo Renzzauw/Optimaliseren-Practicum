93b97a67c604b6c23be6f40deaee333b1df4158c
bennie010697 <bennie.veldhuijzen@gmail.com>
2018-11-21 17:37:39 +0100

    :100: :feelsgood:  :fire:  :gun: Ez no error

--- ConsoleApp/SRC/Program.cs
+++ ConsoleApp/SRC/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace OptimaliserenPracticum
 {
@@ -15,8 +16,42 @@ namespace OptimaliserenPracticum
             SimulatedAnnealing SA = new SimulatedAnnealing();
             SA.InitDiagnostics();
             SA.Init();
+            OrdInit OI = new OrdInit();
+            OI.makeOrder();
         }
     }
+
+    class OrdInit
+    {
+        public List<Order> OrderList = new List<Order>();
+
+
+
+        public void makeOrder()
+        {
+            foreach (string line in File.ReadLines(Directory.GetCurrentDirectory() + "ord.txt", Encoding.UTF8))
+            {
+                string[] words = line.Split(';');
+                int ordernumb = int.Parse(words[0]);
+                string place = words[1];
+                int freq = 1;                                //Dit wordt nog aangepast (met enum van Renzo :^|)
+                int ContCount = int.Parse(words[3]);
+                int volumCont = int.Parse(words[4]);
+                float emptTime = float.Parse(words[5]);
+                int matrixID = int.Parse(words[6]);
+                int xCoord = int.Parse(words[7]);
+                int yCoord = int.Parse(words[8]);
+
+                Order ord = new Order(ordernumb, place, freq, ContCount, volumCont, emptTime, matrixID, xCoord, yCoord);
+
+                OrderList.Add(ord);
+            }
+        }
+    }
+
+
+
+
 
     class SimulatedAnnealing
     {
