3959577297e4a347cdaf3d90c748d40c058ce5fb
Renzzauw <renzo@schindeler.nl>
2018-11-21 16:17:44 +0100

    Constructors van objecten

--- ConsoleApp/GarbageTruck.cs
+++ ConsoleApp/GarbageTruck.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace OptimaliserenPracticum
 {
+	// Garbage truck object
 	public class GarbageTruck
 	{
+		// Variables
 		public const int capacity = 20000;	// Capacity of the truck in liters
-		public const int emptyTime = 30;	// Time to empty garbage truck in minutes
+		public const int emptyTime = 30;    // Time to empty garbage truck in minutes
+		public int currentCapacity;			// Current content of the truck in liters
 
 		// Constructor
-		public GarbageTruck()
+		public GarbageTruck(int currentCapacity = 0)
 		{
-
+			this.currentCapacity = currentCapacity;
 		}
 
 		// Function that handles the emptying of the garbage truck
@@ -22,14 +25,5 @@ namespace ConsoleApp
 		{
 
 		}
-
-		// Punish for skipping delivery (not sure of hij hier moet)
-		public void Punish()
-		{
-
-		}
-		
-
-
 	}
 }
