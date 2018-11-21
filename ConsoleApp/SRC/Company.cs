cda91a8745ea96fd8db94772ab526b3f7efe4f1f
Renzzauw <renzo@schindeler.nl>
2018-11-21 17:33:44 +0100

    Company changes

--- ConsoleApp/SRC/Company.cs
+++ ConsoleApp/SRC/Company.cs
@@ -6,21 +6,59 @@ using System.Threading.Tasks;
 
 namespace OptimaliserenPracticum
 {
+	// Enumerator
+	enum CollectionCounts { Once, Twice };
+
+	// int met bitshiften per dag 0 of 1 langsgeweest
+	enum CollectionPattern { Once, Twice };
+
 	// Company object
 	public class Company
 	{
 		// Variables
-		public int companyIndex;
-		public int xCoordinate, yCoordinate;
-		public string placeName;
-		
+		public int companyIndex;				// Identifier for this company
+		public int xCoordinate, yCoordinate;    // Position of the company
+		public int visitDays = 0;				// Binary representation of the days the company has been visited 
+		public string placeName;				// Name of the place where this company is
+		public Order[] orders;                  // The list of orders of the company
+
 		// Constructor
-		public Company(int companyIndex, int xCoordinate, int yCoordinate, string placeName)
+		public Company(int companyIndex, int xCoordinate, int yCoordinate, string placeName, Order[] orders)
 		{
 			this.companyIndex = companyIndex;
 			this.xCoordinate = xCoordinate;
 			this.yCoordinate = yCoordinate;
 			this.placeName = placeName;
+			this.orders = orders;
 		}
+
+		// When visiting the company, set the bit of this day to 1
+		// dayIndex: monday = 0, tuesday = 1, ...
+		public void VisitCompany(int dayIndex)
+		{
+			// Shift dayIndex to the right position
+			int bit = 16 >> dayIndex;
+			// Perform OR operation to set the bit to true
+			visitDays = visitDays | bit;
+		}
+
+		// Count the amount of times the company has been visited
+		public int GetVisitedDaysCount()
+		{
+			int count = 0;
+			int copy = visitDays;
+			// Count all 1's in the binary representation of visitDays
+			while (copy != 0)
+			{
+				count++;
+				copy &= (copy - 1);
+			}
+
+			return count;
+
+		}
+
+
+
 	}
 }
