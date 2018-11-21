465f717504fd650410deef11f9ce7686bf3f8421
Hidde Veer <hidde.veer@gmail.com>
2018-11-21 16:39:58 +0100

    Godver ik haat github (2/2)

--- ConsoleApp/SRC/Initialization.cs
+++ ConsoleApp/SRC/Initialization.cs
@@ -9,35 +9,37 @@ namespace OptimaliserenPracticum
 {
     class Initialization
     {
-        string localPath;
-        string distPath;
+        string localPath, distanceName, orderningName;
+
         public Initialization()
         {
             localPath = Directory.GetCurrentDirectory();
-            distPath = "dist.txt";
+            distanceName = "dist.txt";
+            orderningName = "ord.txt";
         }
 
+        // Function that processes all the data from the dist file
         public Tuple<int[,], int[,]> GetMatrix()
         {
-            string[] input = File.ReadAllLines(localPath + distPath);
+            string[] input = File.ReadAllLines(localPath + distanceName);
             int inputLength = input.Length;
-            int start, end, dist, time;
             int[,] distMatrix = new int[inputLength, inputLength];
             int[,] timeMatrix = new int[inputLength, inputLength];
-            // The dist/time from each company to itself = 0;
+            // The distance and time from a destination to itself is 0
             for (int i = 0; i < inputLength; i++)
             {
                 distMatrix[i, i] = 0;
                 timeMatrix[i, i] = 0;
             }
-            // Read each line one by one and process it
+            // Take all lines one by one, and process them
+            int start, end, dist, time;
             for (int i = 0; i < inputLength; i++)
             {
-                string[] line = input[i].Split(';');
-                start = int.Parse(line[0]);
-                end = int.Parse(line[1]);
-                dist = int.Parse(line[2]);
-                time = int.Parse(line[3]);
+                string[] data = input[i].Split(';');
+                start = int.Parse(data[0]);
+                end = int.Parse(data[1]);
+                dist = int.Parse(data[2]);
+                time = int.Parse(data[3]);
                 distMatrix[start, end] = dist;
                 timeMatrix[start, end] = time;
             }
