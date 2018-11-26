5dfa8130c2e5174857afc6cf73773854fff76ece
Hidde Veer <hidde.veer@gmail.com>
2018-11-26 13:41:09 +0100

    🅰ctione

--- ConsoleApp/SRC/State.cs
+++ ConsoleApp/SRC/State.cs
@@ -10,32 +10,67 @@ namespace OptimaliserenPracticum
     public class State
     {
         // Variables
-        Company[] companyList;
-        Company hq;
-        enum Action {Driving, Emptying, Collecting, Nothing}
-        Dictionary<decimal, Status>[] status1, status2; // The week for each truck: It contains 5 dictionaries, one for each day. Each truck gets their own status
-        Dictionary<decimal, Status> day; // The status struct keeps track of what the truck is doing at that time moment
+        List<Status>[] status1, status2; // The week for each truck: It contains 5 dictionaries, one for each day. Each truck gets their own status
+         
 		
-		// Make a (random) initial state
+        // Make a (random) initial state
 		public State()
 		{
-            status1 = status2 = new Dictionary<decimal, Status>[5];
+            status1 = MakeRandomState(new GarbageTruck());
+            status2 = MakeRandomState(new GarbageTruck());
+        }
 
-		}
+        public List<Status>[] MakeRandomState(GarbageTruck truck)
+        {
+            List<Status>[] statusList = new List<Status>[5];
+            for(int i = 0; i < 5; i++)
+            {
+                statusList[i] = MakeRandomDay(truck);
+            }
+        }
+
+        public List<Status> MakeRandomDay(GarbageTruck truck)
+        {
+            Random random = new Random(); // A random that we will use here and there
+            List<Status> day = new List<Status>();
+            day.Add(new Status(0, 600, 4, /* Insert haarmeze hier*/ , truck));
+        }
 
 
-		public void GetCompanies()
-		{
 
-		}
-
-
-	}
-    public struct Status
+    }
+    public class Status
     {
-        Action action;
-        Company company;
-        GarbageTruck truck;
+        public int startTime, endTime;
+        public enum Actione { Driving, Collecting, Emptying, Nothing } // Added the extra E to avoid conflicts with System.Action
+        public Actione action;
+        public Company company;
+        public GarbageTruck truck;
+
+        public Status(int s, int e, int z, Company c, GarbageTruck gt)
+        {
+            startTime = s;
+            endTime   = e;
+            switch (z)
+            {
+                case 1:
+                    action = Actione.Driving;
+                    break;
+                case 2:
+                    action = Actione.Collecting;
+                    break;
+                case 3:
+                    action = Actione.Emptying;
+                    break;
+                case 4:
+                    action = Actione.Nothing;
+                    break;
+                default:
+                    break;
+            }
+            company   = c;
+            truck     = gt;
+        }
     }
 
 }
